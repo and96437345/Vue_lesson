978e2448753c94b2d182d144405dadd1b4838ec6
Andrey <136622037+and96437345@users.noreply.github.com>
2023-12-11 21:13:44 +0300

    Vue lesson

---
+++ vue-project/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Card from './components/Card.vue'
+</script>
+
+<template>
+  <Card title="Рубашка 1" :price="30000" />
+  <Card title="Рубашка 1" :price="30000" />
+  <Card title="Рубашка 1" :price="30000" />
+  <Card title="Рубашка 1" :price="30000" />
+  <Card title="Рубашка 1" :price="30000" />
+</template>
+
+<style>
+body {
+  /* background-color: #000; */
+  /* color: #fff; */
+}
+</style>
---
+++ vue-project/src/components/Card.vue
@@ -0,0 +1,40 @@
+<script setup>
+export default {
+  data() {
+    return {
+      count: 0
+    }
+  },
+  methods: {
+    inc() {
+      this.count++
+    }
+  }
+}
+import { Ref } from 'vue'
+let count = ref(0)
+function inc() {
+  count.value++
+}
+
+defineProps({
+  title: String,
+  price: Number,
+  isAdded: Boolean
+})
+</script>
+
+<template>
+  <div class="card">
+    <svg>
+      <use></use>
+    </svg>
+    <div class="card__title">{{ title }}</div>
+    <div class="card__price">{{ price }}</div>
+    <!-- <h2>{{ count }}</h2>
+    <button @click="inc()">Добавить 1</button>
+    <input v-modal="count" /> -->
+  </div>
+</template>
+
+<style></style>
